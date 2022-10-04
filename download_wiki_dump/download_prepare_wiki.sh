ad5bc9375e05f64a3d1b043088695db061f44faf
Anton Alekseev <anton.m.alexeyev@gmail.com>
2022-10-04 13:31:38 +0300

    Update download_prepare_wiki.sh

--- download_wiki_dump/download_prepare_wiki.sh
+++ download_wiki_dump/download_prepare_wiki.sh
@@ -23,14 +23,14 @@ git clone https://github.com/attardi/wikiextractor.git
 # extract and clean the chosen Wikipedia dump
 echo "Extracting and cleaning $WIKI_DUMP_FILE_IN to $WIKI_DUMP_FILE_OUT..."
 
-python3 wikiextractor/WikiExtractor.py $WIKI_DUMP_FILE_IN --processes 8 -q -o - \
+python3 wikiextractor/wikiextractor/WikiExtractor.py $WIKI_DUMP_FILE_IN --processes 8 -q -o - \
 | sed "/^\s*\$/d" \
 | grep -v "^<doc id=" \
 | grep -v "</doc>\$" \
 > $WIKI_DUMP_FILE_OUT
 echo "Succesfully extracted and cleaned $WIKI_DUMP_FILE_IN to $WIKI_DUMP_FILE_OUT"
 
-python3 wikiextractor/WikiExtractor.py $WIKI_DUMP_NAME --processes 8 -q -o - | sed "/^\s*\$/d" \
+python3 wikiextractor/wikiextractor/WikiExtractor.py $WIKI_DUMP_NAME --processes 8 -q -o - | sed "/^\s*\$/d" \
 | grep -v "^<doc id=" \
 | grep -v "</doc>\$" \
 > $WIKI_DUMP_FILE_OUT
